4e205b7dc09c4dad5d08638bc75bc96c82bb738b
michaelmwhite <michael.m.white@live.com>
2020-01-01 20:26:26 -0800

    added header to stats screen

--- app/src/main/java/com/tmacstudios/nbabuddy/stats_screen/StatsActivity.kt
+++ app/src/main/java/com/tmacstudios/nbabuddy/stats_screen/StatsActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.tmacstudios.nbabuddy.R
 import com.tmacstudios.nbabuddy.models.Game
 import com.tmacstudios.nbabuddy.utils.loadBoxscore
+import com.tmacstudios.nbabuddy.views.TeamInfoView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -14,12 +15,20 @@ import kotlinx.coroutines.withContext
 const val EXTRA_GAME = "GAME"
 
 class StatsActivity : AppCompatActivity() {
+    private lateinit var hTeamInfoView: TeamInfoView
+    private lateinit var vTeamInfoView: TeamInfoView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
 
+        hTeamInfoView = findViewById(R.id.h_team_info)
+        vTeamInfoView = findViewById(R.id.v_team_info)
+
         val game = intent.getSerializableExtra(EXTRA_GAME) as Game
+        hTeamInfoView.setTeam(game.hTeam)
+        vTeamInfoView.setTeam(game.vTeam)
+
         GlobalScope.launch {
             try {
                 val boxscore = loadBoxscore(game)!!
@@ -37,6 +46,7 @@ class StatsActivity : AppCompatActivity() {
                         "Unable to get the box score for this game.",
                         Toast.LENGTH_SHORT
                     ).show()
+                    finish()
                 }
             }
         }
